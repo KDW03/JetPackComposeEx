8b09a011ae34719c61940d72e112febe51d92a81
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-06-23 23:33:54 +0900

    first commit

---
+++ app/src/main/java/com/example/jetpackcomposeex/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.jetpackcomposeex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposeex.ui.theme.JetPackComposeExTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            JetPackComposeExTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+
+
+                }
+            }
+        }
+
+
+    }
+}
+
