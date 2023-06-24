437d5686d235aaf1638eeb40a3e1983942ba5fa0
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-06-24 19:17:56 +0900

    Modifier 사용 (width,height,size,clip)

--- app/src/main/java/com/example/jetpackcomposeex/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeex/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.jetpackcomposeex
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,7 +15,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposeex.ui.theme.JetPackComposeExTheme
@@ -24,7 +29,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Text(text = "Hello World")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
@@ -36,6 +41,35 @@ fun MessageCard(name: String) {
     Text(text = "Hello $name")
 }
 
+/** 함수 이름 같더라도 변수 타입 및 개수가 다르면 가능*/
+@Composable
+fun MessageCard(msg: Message) {
+    /** 요소를 수직으로 정렬 Column*/
+    /** 요소를 수평으로 정렬 Row*/
+    /** 요소를 쌓으려면 Box w*/
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        /** Modifider라는 수정자를 통해 컴포저블의 크기, 레이아웃, 모양을 변경하거나 요소를 클릭 가능하게 만드는 등의 상위 수준 상호작용 추가*/
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = " Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+}
+
+
+data class Message(val author: String, val body: String)
 
 /** @Preview 주석을 사용하여 앱을 빌드해서 Android 기기나 애뮬레이터에 설치하지 않고 구성 가능한 함수를 미리 볼 수 있다.
  *  이 주석은 매개변수를 사용하지 않는 구성 가능한 함수에 사용
@@ -45,3 +79,14 @@ fun MessageCard(name: String) {
 fun PreviewMessageCard() {
     MessageCard(name = "Android")
 }
+
+/** Preview도 여러개 가능 */
+@Preview
+@Composable
+fun preViewMessageCard() {
+    MessageCard(msg = Message("Colleague", "Hey, take a look at Jetpack Compose"))
+}
+
+
+
+
