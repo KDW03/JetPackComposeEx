9d0f36a11228b67feea57f2ab1922fd5b7d594d2
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-06-24 20:49:39 +0900

    LazyColumn , LazyRow , Animation 적용

--- app/src/main/java/com/example/jetpackcomposeex/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeex/MainActivity.kt
@@ -5,17 +5,18 @@ import android.os.Bundle
 import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,10 +24,22 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposeex.ui.SampleData
 import com.example.jetpackcomposeex.ui.theme.JetPackComposeExTheme
 
 var counter = 0
 
+
+/**
+ *  로컬 UI 상태를 저장하려면 메시지가 확장되었는지 추적
+ *  이 상태 변경을 추적하려면 remember와 mutableStateOf 함수를 사용
+ *
+ *  구성 가능한 함수는 remember를 사용하여 메모리에 로컬 상태를 저장
+ *  mutableStateOf에 전달된 값의 변경사항을 추적
+ *
+ * 이 상태를 사용하는 컴포저블 및 하위 요소는 값이 업데이트되면 자동으로 다시 그려짐 = 재구성
+ * remember 및 mutableStateOf와 같은 Compose의 상태 API를 사용하여 상태를 변경하면 UI가 자동으로 업데이트
+ * */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +49,7 @@ class MainActivity : ComponentActivity() {
             JetPackComposeExTheme {
                 // 배경색, 모양, 그림자 등을 지정하는 데 사용되는 컴포저블
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(Message("Android", "Jetpack Compose"))
+                    Conversation(message = SampleData.conversationSample)
                 }
             }
         }
@@ -71,7 +84,20 @@ fun MessageCard(msg: Message) {
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        // We keep track if the message is expanded or not in this
+        //variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+
+        // we toggle the isExpanded variable when we click on this Column
+        Column(
+
+        ) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
@@ -81,14 +107,21 @@ fun MessageCard(msg: Message) {
             // 도형을 적용해주기 위해 surface로 래핑
             Surface(
                 shape = MaterialTheme.shapes.medium, elevation = 1.dp,
-                border = BorderStroke(
-                    3.5.dp,
-                    Color.Blue
-                ),
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier
+                    .clickable { isExpanded = !isExpanded }
+                    .animateContentSize()
+                    .padding(1.dp)
             ) {
                 Text(
                     text = msg.body,
-                    modifier = Modifier.padding(all = 4.dp),
+                    modifier = Modifier
+                        .padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2
                 )
             }
@@ -97,8 +130,18 @@ fun MessageCard(msg: Message) {
     }
 }
 
+/** LazyColumn과 LazyRow를 사용합니다. 이러한 컴포저블은 화면에 표시되는 요소만 렌더링하므로 긴 목록에 매우 효율적으로 설계 */
+@Composable
+fun Conversation(message: List<Message>) {
+    LazyColumn {
+        //items = LazyColumn 하위 요소
+        // List의 항목 마다 호출되는 람다
+        items(message) {
+            MessageCard(msg = it)
+        }
+    }
+}
 
-data class Message(val author: String, val body: String)
 
 /** @Preview 주석을 사용하여 앱을 빌드해서 Android 기기나 애뮬레이터에 설치하지 않고 구성 가능한 함수를 미리 볼 수 있다.
  *  이 주석은 매개변수를 사용하지 않는 구성 가능한 함수에 사용
@@ -125,6 +168,9 @@ fun preViewMessageCard() {
         }
     }
 }
+
+data class Message(val author: String, val body: String)
+
 
 
 
